4fb6f058952b20e5a16e77cc9342e5d5073a4801
Thiago Bastos <thiagosbastos@live.com>
2021-08-20 12:29:59 -0300

    Added printf for opengl errors

--- Assigment1/main.cpp
+++ Assigment1/main.cpp
@@ -2,6 +2,7 @@
 #include <GL/gl.h>
 #include <array>
 #include <cmath>
+#include <cstdio>
 
 namespace cfg {
 static constexpr std::array<int, 2> window_sz = {600, 400};
@@ -10,10 +11,11 @@ static constexpr std::array<float, 3> up_dir = {0.0f, 1.0f, 0.0f};
 static constexpr std::array<float, 4> bg_color = {1.0f, 1.0f, 1.0f, 1.0f};
 static constexpr float cam_angle = 45.0;
 static constexpr float cam_near = 1.0;
-static constexpr float cam_far = 10.0;
+static constexpr float cam_far = 20.0;
+static constexpr auto title = "MyScene";
 }  // namespace cfg
 
-std::array<float, 4> eye = {2.0f, 3.0f, 4.0f, 1.0f};
+std::array<float, 4> eye = {4.0f, 6.0f, 8.0f, 1.0f};
 
 static void setCamera()
 {
@@ -39,9 +41,6 @@ static void drawCube()
 
   // left face:
   glNormal3f(-1.0f, 0.0f, 0.0f);
-  glNormal3f(-1.0f, 0.0f, 0.0f);
-  glNormal3f(-1.0f, 0.0f, 0.0f);
-  glNormal3f(-1.0f, 0.0f, 0.0f);
 
   glVertex3f(-0.5f, -0.5f, -0.5f);
   glVertex3f(-0.5f, -0.5f, 0.5f);
@@ -50,10 +49,6 @@ static void drawCube()
 
   // right face: counter clockwise
   glNormal3f(1.0f, 0.0f, 0.0f);
-  glNormal3f(1.0f, 0.0f, 0.0f);
-  glNormal3f(1.0f, 0.0f, 0.0f);
-  glNormal3f(1.0f, 0.0f, 0.0f);
-
   glVertex3f(0.5f, -0.5f, -0.5f);
   glVertex3f(0.5f, 0.5f, -0.5f);
   glVertex3f(0.5f, 0.5f, 0.5f);
@@ -61,10 +56,6 @@ static void drawCube()
 
   // top face:
   glNormal3f(0.0f, 1.0f, 0.0f);
-  glNormal3f(0.0f, 1.0f, 0.0f);
-  glNormal3f(0.0f, 1.0f, 0.0f);
-  glNormal3f(0.0f, 1.0f, 0.0f);
-
   glVertex3f(0.5f, 0.5f, 0.5f);
   glVertex3f(0.5f, 0.5f, -0.5f);
   glVertex3f(-0.5f, 0.5f, -0.5f);
@@ -72,10 +63,6 @@ static void drawCube()
 
   // bottom face:
   glNormal3f(0.0f, -1.0f, 0.0f);
-  glNormal3f(0.0f, -1.0f, 0.0f);
-  glNormal3f(0.0f, -1.0f, 0.0f);
-  glNormal3f(0.0f, -1.0f, 0.0f);
-
   glVertex3f(-0.5f, -0.5f, -0.5f);
   glVertex3f(0.5f, -0.5f, -0.5f);
   glVertex3f(0.5f, -0.5f, 0.5f);
@@ -83,26 +70,18 @@ static void drawCube()
 
   // front face:
   glNormal3f(0.0f, 0.0f, 1.0f);
-  glNormal3f(0.0f, 0.0f, 1.0f);
-  glNormal3f(0.0f, 0.0f, 1.0f);
-  glNormal3f(0.0f, 0.0f, 1.0f);
-
   glVertex3f(-0.5f, -0.5f, 0.5f);
   glVertex3f(0.5f, -0.5f, 0.5f);
   glVertex3f(0.5f, 0.5f, 0.5f);
   glVertex3f(-0.5f, 0.5f, 0.5f);
-
+  
   // back face:
   glNormal3f(0.0f, 0.0f, -1.0f);
-  glNormal3f(0.0f, 0.0f, -1.0f);
-  glNormal3f(0.0f, 0.0f, -1.0f);
-  glNormal3f(0.0f, 0.0f, -1.0f);
-
   glVertex3f(-0.5f, -0.5f, -0.5f);
   glVertex3f(-0.5f, 0.5f, -0.5f);
   glVertex3f(0.5f, 0.5f, -0.5f);
   glVertex3f(0.5f, -0.5f, -0.5f);
-
+  
   glEnd();
 }
 
@@ -119,44 +98,47 @@ static void drawSphere(int n_lat, int n_long)
   for (int j = 0; j < n_long; ++j) {
     for (int i = 0; i <= n_lat; ++i) {
       std::array<float, 3> v0 = {cosf(i * dtheta) * sinf(j * dphi), cosf(j * dphi),
-                       sinf(i * dtheta) * sinf(j * dphi)};
+                                 sinf(i * dtheta) * sinf(j * dphi)};
 
       std::array<float, 3> v1 = {cosf((i + 1) * dtheta) * sinf((j + 1) * dphi),
-                       cosf((j + 1) * dphi),
-                       sinf((i + 1) * dtheta) * sinf((j + 1) * dphi)};
+                                 cosf((j + 1) * dphi),
+                                 sinf((i + 1) * dtheta) * sinf((j + 1) * dphi)};
 
       std::array<float, 3> v2 = {cosf((i + 1) * dtheta) * sinf(j * dphi), cosf(j * dphi),
-                       sinf((i + 1) * dtheta) * sinf(j * dphi)};
+                                 sinf((i + 1) * dtheta) * sinf(j * dphi)};
 
       std::array<float, 3> v3 = {cosf(i * dtheta) * sinf(j * dphi), cosf(j * dphi),
-                       sinf(i * dtheta) * sinf(j * dphi)};
+                                 sinf(i * dtheta) * sinf(j * dphi)};
 
-      std::array<float, 3> v4 = {cosf(i * dtheta) * sinf((j + 1) * dphi), cosf((j + 1) * dphi),
-                       sinf(i * dtheta) * sinf((j + 1) * dphi)};
+      std::array<float, 3> v4 = {cosf(i * dtheta) * sinf((j + 1) * dphi),
+                                 cosf((j + 1) * dphi),
+                                 sinf(i * dtheta) * sinf((j + 1) * dphi)};
 
       std::array<float, 3> v5 = {cosf((i + 1) * dtheta) * sinf((j + 1) * dphi),
-                       cosf((j + 1) * dphi),
-                       sinf((i + 1) * dtheta) * sinf((j + 1) * dphi)};
+                                 cosf((j + 1) * dphi),
+                                 sinf((i + 1) * dtheta) * sinf((j + 1) * dphi)};
 
       std::array<float, 3> n0 = {(sinf(j * dphi) * cosf(i * dtheta)), (cosf(j * dphi)),
-                       (sinf(j * dphi) * sinf(i * dtheta))};
+                                 (sinf(j * dphi) * sinf(i * dtheta))};
 
       std::array<float, 3> n1 = {(sinf((j + 1) * dphi) * cosf((i + 1) * dtheta)),
-                       (cosf((j + 1) * dphi)),
-                       (sinf((j + 1) * dphi) * sinf((i + 1) * dtheta))};
+                                 (cosf((j + 1) * dphi)),
+                                 (sinf((j + 1) * dphi) * sinf((i + 1) * dtheta))};
 
-      std::array<float, 3> n2 = {(sinf(j * dphi) * cosf((i + 1) * dtheta)), (cosf(j * dphi)),
-                       (sinf(j * dphi) * sinf((i + 1) * dtheta))};
+      std::array<float, 3> n2 = {(sinf(j * dphi) * cosf((i + 1) * dtheta)),
+                                 (cosf(j * dphi)),
+                                 (sinf(j * dphi) * sinf((i + 1) * dtheta))};
 
       std::array<float, 3> n3 = {(sinf(j * dphi) * cosf(i * dtheta)), (cosf(j * dphi)),
-                       (sinf(j * dphi) * sinf(i * dtheta))};
+                                 (sinf(j * dphi) * sinf(i * dtheta))};
 
-      std::array<float, 3> n4 = {(sinf((j + 1) * dphi) * cosf(i * dtheta)), (cosf((j + 1) * dphi)),
-                       (sinf((j + 1) * dphi) * sinf(i * dtheta))};
+      std::array<float, 3> n4 = {(sinf((j + 1) * dphi) * cosf(i * dtheta)),
+                                 (cosf((j + 1) * dphi)),
+                                 (sinf((j + 1) * dphi) * sinf(i * dtheta))};
 
       std::array<float, 3> n5 = {(sinf((j + 1) * dphi) * cosf((i + 1) * dtheta)),
-                       (cosf((j + 1) * dphi)),
-                       (sinf((j + 1) * dphi) * sinf((i + 1) * dtheta))};
+                                 (cosf((j + 1) * dphi)),
+                                 (sinf((j + 1) * dphi) * sinf((i + 1) * dtheta))};
 
       glVertex3f(v0[0], v0[1], v0[2]);
       glVertex3f(v1[0], v1[1], v1[2]);
@@ -178,23 +160,87 @@ static void drawSphere(int n_lat, int n_long)
 
 static void drawScene()
 {
-  std::array<float, 4> amb = {0.1f, 0.1f, 0.1f, 1.0f};
-  std::array<float, 4> dif = {0.7f, 0.7f, 0.7f, 1.0f};
-  std::array<float, 4> spe = {1.0f, 1.0f, 1.0f, 1.0f};
+  constexpr std::array<float, 4> amb = {0.1f, 0.1f, 0.1f, 1.0f};
+  constexpr std::array<float, 4> dif = {0.7f, 0.7f, 0.7f, 1.0f};
+  constexpr std::array<float, 4> spe = {1.0f, 1.0f, 1.0f, 1.0f};
 
   glLightfv(GL_LIGHT0, GL_POSITION, eye.data());
   glLightfv(GL_LIGHT0, GL_AMBIENT, amb.data());
   glLightfv(GL_LIGHT0, GL_DIFFUSE, dif.data());
   glLightfv(GL_LIGHT0, GL_SPECULAR, spe.data());
 
-  std::array<float, 4> red = {1.0f, 0.0f, 0.0f, 1.0f};
-  std::array<float, 4> white = {1.0f, 1.0f, 1.0f, 1.0f};
+  constexpr std::array<float, 4> red = {1.0f, 0.0f, 0.0f, 1.0f};
+  constexpr std::array<float, 4> white = {1.0f, 1.0f, 1.0f, 1.0f};
+  constexpr std::array<float, 4> gray = {0.8f, 0.8f, 0.8f, 1.0f};
+  constexpr std::array<float, 4> green = {0.0f, 1.0f, 0.0f, 1.0f};
+  constexpr std::array<float, 4> blue = {0.0f, 0.0f, 1.0f, 1.0f};
 
-  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, red.data());
+  // glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, red.data());
   glMaterialfv(GL_FRONT, GL_SPECULAR, white.data());
   glMaterialf(GL_FRONT, GL_SHININESS, 50.0f);
+  
+  // Table top
+  glPushMatrix();
+  glScalef(5.0f, 0.2f, 5.0f);
+  glTranslatef(0.0f, -0.5f, 0.0f);
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, gray.data());
+  drawCube();
+  glPopMatrix();
 
-  drawSphere(64, 64);
+  // Left/back table foot
+  glPushMatrix();
+  glScalef(0.75f, 3.0f, 0.75f);
+  glTranslatef(-2.25f, -0.5f, -2.25f);
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, gray.data());
+  drawCube();
+  glPopMatrix();
+
+  // Left/front table foot
+  glPushMatrix();
+  glScalef(0.75f, 3.0f, 0.75f);
+  glTranslatef(-2.25f, -0.5f, 2.25f);
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, gray.data());
+  drawCube();
+  glPopMatrix();
+
+  // Right/back table foot
+  glPushMatrix();
+  glScalef(0.75f, 3.0f, 0.75f);
+  glTranslatef(2.25f, -0.5f, -2.25f);
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, gray.data());
+  drawCube();
+  glPopMatrix();
+
+  // Right/front table foot
+  glPushMatrix();
+  glScalef(0.75f, 3.0f, 0.75f);
+  glTranslatef(2.25f, -0.5f, 2.25f);
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, gray.data());
+  drawCube();
+  glPopMatrix();
+
+  // Red cube on top of the table
+  glPushMatrix();
+  glTranslatef(0.5f, 0.5f, -0.5f);
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, red.data());
+  drawCube();
+  glPopMatrix();
+
+  // Green sphere on top of the table
+  glPushMatrix();
+  glScalef(0.5f, 0.5f, 0.5f);
+  glTranslatef(-1.5f, 1.0f, +1.0f);
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, green.data());
+  drawSphere(128, 128);
+  glPopMatrix();
+
+  // Blue sphere on top of the table
+  glPushMatrix();
+  glScalef(0.5f, 0.5f, 0.5f);
+  glTranslatef(2.5f, 1.0f, 1.0f);
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, blue.data());
+  drawSphere(128, 128);
+  glPopMatrix();
 }
 
 static void myDisplayCB()
@@ -203,6 +249,10 @@ static void myDisplayCB()
   setCamera();
   drawScene();
   glutSwapBuffers();  // update screen
+
+  auto error = glGetError();
+  if (error)
+    printf("Error %d", error);
 }
 
 static void myKeyboardCB(uint8_t key, int x, int y)
@@ -237,6 +287,9 @@ static void initialize()
 
   // enable light source number 0
   glEnable(GL_LIGHT0);
+
+  // renormalize the vectors after transformations
+  glEnable(GL_NORMALIZE);
 }
 
 int main(int argc, char** argv)
@@ -245,9 +298,9 @@ int main(int argc, char** argv)
   glutInit(&argc, argv);
 
   // configure and create window
-  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
   glutInitWindowSize(cfg::window_sz[0], cfg::window_sz[1]);
-  glutCreateWindow("MyScene");
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);  
+  glutCreateWindow(cfg::title);
 
   glutDisplayFunc(myDisplayCB);
   glutKeyboardFunc(myKeyboardCB);
