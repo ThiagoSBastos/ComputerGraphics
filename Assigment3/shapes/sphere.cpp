cd3a5925d2d8f31f3a3b75ee5fc798813430004c
Thiago Bastos <thiagosbastos@live.com>
2021-08-30 11:34:58 -0300

    Clean up sphere.cpp

--- Assigment3/shapes/sphere.cpp
+++ Assigment3/shapes/sphere.cpp
@@ -5,9 +5,37 @@
 
 namespace {
 constexpr float m_pi = 3.1415926535897932384626433832795028;
+
+auto getVertexArr = [](int i, int j, float dtheta, float dphi) {
+  std::array<float, 18> v_arr = {
+    cosf(i * dtheta) * sinf(j * dphi), cosf(j * dphi), sinf(i * dtheta) * sinf(j * dphi),
+    cosf((i + 1) * dtheta) * sinf((j + 1) * dphi), cosf((j + 1) * dphi), sinf((i + 1) * dtheta) * sinf((j + 1) * dphi),
+    cosf((i + 1) * dtheta) * sinf(j * dphi), cosf(j * dphi), sinf((i + 1) * dtheta) * sinf(j * dphi),
+    cosf(i * dtheta) * sinf(j * dphi), cosf(j * dphi), sinf(i * dtheta) * sinf(j * dphi),
+    cosf(i * dtheta) * sinf((j + 1) * dphi), cosf((j + 1) * dphi), sinf(i * dtheta) * sinf((j + 1) * dphi),
+    cosf((i + 1) * dtheta) * sinf((j + 1) * dphi), cosf((j + 1) * dphi), sinf((i + 1) * dtheta) * sinf((j + 1) * dphi)
+  };
+
+  return v_arr;
+};
+
+auto getNormalArr = [](int i, int j, float dtheta, float dphi) {
+  std::array<float, 18> n_arr = {
+    (sinf(j * dphi) * cosf(i * dtheta)), (cosf(j * dphi)), (sinf(j * dphi) * sinf(i * dtheta)),
+    (sinf((j + 1) * dphi) * cosf((i + 1) * dtheta)), (cosf((j + 1) * dphi)), (sinf((j + 1) * dphi) * sinf((i + 1) * dtheta)),
+    (sinf(j * dphi) * cosf((i + 1) * dtheta)), (cosf(j * dphi)), (sinf(j * dphi) * sinf((i + 1) * dtheta)),
+    (sinf(j * dphi) * cosf(i * dtheta)), (cosf(j * dphi)), (sinf(j * dphi) * sinf(i * dtheta)),
+    (sinf((j + 1) * dphi) * cosf(i * dtheta)), (cosf((j + 1) * dphi)), (sinf((j + 1) * dphi) * sinf(i * dtheta)),
+    (sinf((j + 1) * dphi) * cosf((i + 1) * dtheta)), (cosf((j + 1) * dphi)), (sinf((j + 1) * dphi) * sinf((i + 1) * dtheta))
+  };
+
+  return n_arr;
+};
+
 }
 
 Sphere::Sphere(int nlong, int nlat) : m_nlong(nlong), m_nlat(nlat) {}
+
 
 void Sphere::draw() const
 {
@@ -17,62 +45,22 @@ void Sphere::draw() const
   glBegin(GL_TRIANGLES);
   for (int j = 0; j < m_nlong; ++j) {
     for (int i = 0; i <= m_nlat; ++i) {
-      std::array<float, 3> v0 = {cosf(i * dtheta) * sinf(j * dphi), cosf(j * dphi),
-                                 sinf(i * dtheta) * sinf(j * dphi)};
+      auto v_arr = getVertexArr(i, j, dtheta, dphi);
+      auto n_arr = getNormalArr(i, j, dtheta, dphi);
+      
+      glVertex3f(v_arr[0] , v_arr[1] , v_arr[2]);
+      glVertex3f(v_arr[3] , v_arr[4] , v_arr[5]);
+      glVertex3f(v_arr[6] , v_arr[7] , v_arr[8]);
+      glVertex3f(v_arr[9] , v_arr[10], v_arr[11]);
+      glVertex3f(v_arr[12], v_arr[13], v_arr[14]);
+      glVertex3f(v_arr[15], v_arr[16], v_arr[17]);
 
-      std::array<float, 3> v1 = {cosf((i + 1) * dtheta) * sinf((j + 1) * dphi),
-                                 cosf((j + 1) * dphi),
-                                 sinf((i + 1) * dtheta) * sinf((j + 1) * dphi)};
-
-      std::array<float, 3> v2 = {cosf((i + 1) * dtheta) * sinf(j * dphi), cosf(j * dphi),
-                                 sinf((i + 1) * dtheta) * sinf(j * dphi)};
-
-      std::array<float, 3> v3 = {cosf(i * dtheta) * sinf(j * dphi), cosf(j * dphi),
-                                 sinf(i * dtheta) * sinf(j * dphi)};
-
-      std::array<float, 3> v4 = {cosf(i * dtheta) * sinf((j + 1) * dphi),
-                                 cosf((j + 1) * dphi),
-                                 sinf(i * dtheta) * sinf((j + 1) * dphi)};
-
-      std::array<float, 3> v5 = {cosf((i + 1) * dtheta) * sinf((j + 1) * dphi),
-                                 cosf((j + 1) * dphi),
-                                 sinf((i + 1) * dtheta) * sinf((j + 1) * dphi)};
-
-      std::array<float, 3> n0 = {(sinf(j * dphi) * cosf(i * dtheta)), (cosf(j * dphi)),
-                                 (sinf(j * dphi) * sinf(i * dtheta))};
-
-      std::array<float, 3> n1 = {(sinf((j + 1) * dphi) * cosf((i + 1) * dtheta)),
-                                 (cosf((j + 1) * dphi)),
-                                 (sinf((j + 1) * dphi) * sinf((i + 1) * dtheta))};
-
-      std::array<float, 3> n2 = {(sinf(j * dphi) * cosf((i + 1) * dtheta)),
-                                 (cosf(j * dphi)),
-                                 (sinf(j * dphi) * sinf((i + 1) * dtheta))};
-
-      std::array<float, 3> n3 = {(sinf(j * dphi) * cosf(i * dtheta)), (cosf(j * dphi)),
-                                 (sinf(j * dphi) * sinf(i * dtheta))};
-
-      std::array<float, 3> n4 = {(sinf((j + 1) * dphi) * cosf(i * dtheta)),
-                                 (cosf((j + 1) * dphi)),
-                                 (sinf((j + 1) * dphi) * sinf(i * dtheta))};
-
-      std::array<float, 3> n5 = {(sinf((j + 1) * dphi) * cosf((i + 1) * dtheta)),
-                                 (cosf((j + 1) * dphi)),
-                                 (sinf((j + 1) * dphi) * sinf((i + 1) * dtheta))};
-
-      glVertex3f(v0[0], v0[1], v0[2]);
-      glVertex3f(v1[0], v1[1], v1[2]);
-      glVertex3f(v2[0], v2[1], v2[2]);
-      glVertex3f(v3[0], v3[1], v3[2]);
-      glVertex3f(v4[0], v4[1], v4[2]);
-      glVertex3f(v5[0], v5[1], v5[2]);
-
-      glNormal3f(n0[0], n0[1], n0[2]);
-      glNormal3f(n1[0], n1[1], n1[2]);
-      glNormal3f(n2[0], n2[1], n2[2]);
-      glNormal3f(n3[0], n3[1], n3[2]);
-      glNormal3f(n4[0], n4[1], n4[2]);
-      glNormal3f(n5[0], n5[1], n5[2]);
+      glNormal3f(n_arr[0], n_arr[1], n_arr[2]);
+      glNormal3f(n_arr[3], n_arr[4], n_arr[5]);
+      glNormal3f(n_arr[6], n_arr[7], n_arr[8]);
+      glNormal3f(n_arr[9], n_arr[10], n_arr[11]);
+      glNormal3f(n_arr[12], n_arr[13], n_arr[14]);
+      glNormal3f(n_arr[15], n_arr[16], n_arr[17]);
     }
   }
   glEnd();
